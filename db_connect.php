7f732e7c391d3f13bceb47ee24731d91237d8aee
Radhika Garg <radhikagarg2805@gmail.com>
2023-10-01 00:05:31 -0700

    commit

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$server_name = "localhost";
+$user_name = 'root';
+$user_password = '';
+$database_name = "music_user";
+
+$con = mysqli_connect($server_name, $user_name, $user_password, $database_name);
+
+if (!$con) {
+    die ('Connection Failed');
+} 
+
+?>
